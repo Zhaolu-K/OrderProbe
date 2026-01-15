32870c52bfd3d4c24f8a1cbd81d36957af3c88d0
szx1128 <1230503008@cnu.edu.cn>
2026-01-15 23:55:06 +0800

    Delete orderprobe directory

--- orderprobe/Slogic/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Logical Validity (S_Log) Module
-
-Implements S_Log evaluation using Natural Language Inference (NLI)
-to assess logical consistency between predictions and references.
-"""
-
-from .s_log_calculator import *
