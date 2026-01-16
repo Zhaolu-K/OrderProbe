04e2bdeb324404ed3365928c33f4649398ca1e62
szx1128 <1230503008@cnu.edu.cn>
2026-01-16 18:58:11 +0800

    Delete Sinfo directory

--- Sinfo/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Information Density (S_Info) Module
-
-Implements S_Info evaluation using ROUGE precision and brevity penalty
-to reward concise, high-information explanations.
-"""
-
-from .s_info_calculator import *
